45d90f6119f1f7749ea009d2e7b50d4164792cbf
Admin <mihailbichkov@yandex.ru>
2020-04-19 23:34:46 +0500

    Initial commit

---
+++ src/main/java/ru/job4j/App.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ mvn archetype:generate -DgroupId=ru.job4j -DartifactId=job4j_design -DarchetypeArtifactId=maven-archetype-simple -DarchetypeVersion=1.4 -DinteractiveMode=false
+ mvn install
+*/
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
