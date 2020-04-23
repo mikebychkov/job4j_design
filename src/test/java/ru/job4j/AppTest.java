372a00d991062ab64f1365ac5cbfd5b5d8f82df1
Admin <mihailbichkov@yandex.ru>
2020-04-24 00:33:56 +0500

    pom.xml configuring #202004240033

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -1,33 +1,18 @@
 package ru.job4j;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+public class AppTest {
+
+    @Test
+    public void testApp1() {
+        assertThat(false, is(false));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void testApp2() {
+        assertThat(true, is(true));
     }
 }
