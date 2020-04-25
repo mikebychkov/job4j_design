518f11bf616ef41a481cde924fce1a180d0cf16a
Admin <mihailbichkov@yandex.ru>
2020-04-25 20:46:27 +0500

    5.1.2. Создать итератор четные числа[#276420] #202004252046

--- chapter_001/src/main/java/com/iterator/EvenNumbersIterator.java
+++ chapter_001/src/main/java/com/iterator/EvenNumbersIterator.java
@@ -6,22 +6,26 @@ import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator<Integer> {
     private final int[] arr;
-    private int pointer = 0;
+    private int pointerNext = -1;
 
     public EvenNumbersIterator(int[] param) {
-        int[] rsl = new int[param.length];
-        int index = 0;
-        for (int i : param) {
-            if (i % 2 == 0) {
-                rsl[index++] = i;
+        this.arr = param;
+        checkNext();
+    }
+
+    private void checkNext() {
+        for (int i = this.pointerNext + 1; i < this.arr.length; i++) {
+            if (this.arr[i] % 2 == 0) {
+                this.pointerNext = i;
+                return;
             }
         }
-        this.arr = Arrays.copyOf(rsl, index);
+        this.pointerNext = -1;
     }
 
     @Override
     public boolean hasNext() {
-        return this.pointer < this.arr.length;
+        return this.pointerNext >= 0;
     }
 
     @Override
@@ -29,6 +33,8 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException("No elements to provide");
         }
-        return this.arr[this.pointer++];
+        Integer rsl = this.arr[this.pointerNext];
+        checkNext();
+        return rsl;
     }
 }
