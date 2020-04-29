7b3e136ce449e9af8ff9e9d29797e62f61581262
Admin <mihailbichkov@yandex.ru>
2020-04-29 18:56:29 +0500

    1. Создать элементарную структуру дерева [#276448]

---
+++ chapter_001/src/main/java/com/tree/Tree.java
@@ -0,0 +1,37 @@
+package com.tree;
+
+import java.util.*;
+
+class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> pNode = findBy(parent);
+        if (pNode.isEmpty()) {
+            return false;
+        }
+        pNode.get().addChild(new Node<>(child));
+        return true;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
