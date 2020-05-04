b22cb629534336294b307a9690c15280453e6472
Admin <mihailbichkov@yandex.ru>
2020-05-04 17:55:43 +0500

    #202005041755

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  mvn archetype:generate -DgroupId=ru.job4j -DartifactId=job4j_design -DarchetypeArtifactId=maven-archetype-simple -DarchetypeVersion=1.4 -DinteractiveMode=false
@@ -29,6 +29,66 @@ public class App {
             }
         }
 
+        private class InClass2<K, V> {
+            K[] key;
+            V[] val;
+
+            public InClass2() {
+                this.key = (K[]) new Object[47];
+                this.val = (V[]) new Object[47];
+            }
+        }
+
+        private static class SuperInClass3<T extends Number> {
+            T val;
+        }
+
+        private static class InClass3<T extends Number> extends SuperInClass3<T> {
+
+            public void printClass() {
+                Type param = (
+                        (ParameterizedType) getClass().getGenericSuperclass()
+                ).getActualTypeArguments()[0];
+                System.out.println(
+                    param
+                );
+            }
+        }
+    }
+
+    public static class MyClass<A, B, C> {
+
+        Class<A> aType;
+
+        Class<B> bType;
+
+        Class<C> cType;
+
+
+        public MyClass() {
+            this.aType = (Class<A>) getGenericClassType(0);
+            this.bType = (Class<B>) getGenericClassType(1);
+            this.cType = (Class<C>) getGenericClassType(2);
+        }
+
+        /**
+         * Returns a {@link Type} object to identify generic types
+         * @return type
+         */
+        Type getGenericClassType(int index) {
+
+            Type type = getClass().getGenericSuperclass();
+
+            while (!(type instanceof ParameterizedType)) {
+                if (type instanceof ParameterizedType) {
+                    type = ((Class<?>) ((ParameterizedType) type).getRawType()).getGenericSuperclass();
+                } else {
+                    type = ((Class<?>) type).getGenericSuperclass();
+                }
+            }
+
+            return ((ParameterizedType) type).getActualTypeArguments()[index];
+        }
     }
 
     public static void main(String[] args) {
@@ -49,5 +109,24 @@ public class App {
         ArrayList<OutClass> list = new ArrayList<>();
         list.add(new OutClass(7));
         System.out.println(list);
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("computer");
+        System.out.println(list2.get(0).getClass());
+
+        System.out.println(list.get(0).getClass());
+
+        ArrayList<String> list3 = new ArrayList<>();
+        System.out.println(
+                //list3.getClass().getGenericSignature0().getClass()
+        );
+
+        Map<Integer, String> map = new HashMap<>();
+
+        OutClass.InClass3<Integer> ic3 = new OutClass.InClass3<>();
+        ic3.printClass();
+
+//        MyClass<String, Integer, MyClass> mc = new MyClass<>();
+//        System.out.println(mc.aType);
     }
 }
