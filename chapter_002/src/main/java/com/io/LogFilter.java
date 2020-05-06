138dc350c01b81294c390ff84153b0567e10d1a3
Admin <mihailbichkov@yandex.ru>
2020-05-06 23:36:12 +0500

    0.4. BufferedOutputStream [#276463]

--- chapter_002/src/main/java/com/io/LogFilter.java
+++ chapter_002/src/main/java/com/io/LogFilter.java
@@ -1,7 +1,6 @@
 package com.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,10 +20,24 @@ public class LogFilter {
         return null;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String str : log) {
+                out.write(str + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         for (String line : log) {
             System.out.println(line);
         }
+        save(log, "404.txt");
     }
 }
---
+++ chapter_002/src/main/java/com/io/ResultFileBuffered.java
@@ -0,0 +1,19 @@
+package com.io;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+public class ResultFileBuffered {
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.write("Hello, world!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
