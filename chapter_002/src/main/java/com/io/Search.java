24b43e38ca4616e8494531cde8139ff8db6ba866
Admin <mihailbichkov@yandex.ru>
2020-05-08 20:44:09 +0500

    4.1. Сканирование файловой системы. [#276458]

---
+++ chapter_002/src/main/java/com/io/Search.java
@@ -0,0 +1,61 @@
+package com.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class Search {
+
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        //Files.walkFileTree(start, new PrintFiles());
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static class SearchFiles implements FileVisitor<Path> {
+        Predicate<Path> pathPredicate;
+        List<Path> listOfPaths = new ArrayList<>();
+
+        public SearchFiles(Predicate<Path> pathPredicate) {
+            this.pathPredicate = pathPredicate;
+        }
+
+        public List<Path> getPaths() {
+            return this.listOfPaths;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (pathPredicate.test(file)) {
+                this.listOfPaths.add(file);
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
