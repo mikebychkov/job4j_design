9b3ee71a4958425e1a78fc34de65ab2e55028fa0
Admin <mihailbichkov@yandex.ru>
2020-05-09 13:42:44 +0500

    6. Кодировка. [#276468]

---
+++ chapter_002/src/main/java/com/io/ConsoleChat.java
@@ -0,0 +1,81 @@
+package com.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class ConsoleChat {
+    private boolean silentMode;
+    private boolean exitMode;
+    private Random rnd = new Random();
+    private List<String> answers = new ArrayList<>();
+    private List<String> log = new LinkedList<>();
+    private String logFile = "";
+
+    public ConsoleChat(String answersFile, String logFile) {
+        File ansFl = new File(answersFile);
+        if (!ansFl.exists()) {
+            throw new IllegalArgumentException("No such answers file!");
+        }
+        this.answers = readAnswers(answersFile);
+        this.logFile = logFile;
+    }
+
+    public void start() throws IOException {
+        Scanner input = new Scanner(System.in);
+        while (!exitMode) {
+            String userIn = input.nextLine();
+            this.log.add(userIn);
+            if (userIn.equals("стоп")) {
+                this.silentMode = true;
+            } else if (userIn.equals("продолжить")) {
+                this.silentMode = false;
+            } else if (userIn.equals("закончить")) {
+                this.exitMode = true;
+                continue;
+            }
+            if (this.silentMode) {
+                continue;
+            }
+            String outMsg = this.answers.get(this.rnd.nextInt(this.answers.size()));
+            this.log.add(outMsg);
+            System.out.println(outMsg);
+        }
+        writeLog();
+    }
+
+    public List<String> readAnswers(String filename) {
+        List<String> out = new ArrayList<>();
+        try (BufferedReader read = new BufferedReader(new FileReader(filename))) {
+            read.lines().flatMap(
+                    ln -> Stream.of(ln.split(" "))
+            ).forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out;
+    }
+
+    private void writeLog() {
+        if (this.logFile.isEmpty()) {
+            return;
+        }
+        try (PrintWriter out = new PrintWriter(
+                new FileOutputStream(this.logFile)
+        )) {
+            for (String str : this.log) {
+                out.println(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Argument not specified ANSWERS_FILE LOG_FILE");
+        }
+        ConsoleChat cc = new ConsoleChat(args[0], args[1]);
+        cc.start();
+    }
+}
