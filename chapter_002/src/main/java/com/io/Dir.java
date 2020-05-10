9b44216e72b3fd59c764d0b8e5a7e0724c98f40f
Admin <mihailbichkov@yandex.ru>
2020-05-10 21:58:58 +0500

    4.0. File [#276464] #202005102158

--- chapter_002/src/main/java/com/io/Dir.java
+++ chapter_002/src/main/java/com/io/Dir.java
@@ -3,20 +3,37 @@ package com.io;
 import java.io.File;
 
 public class Dir {
+
+    public static long getFileSize(File fl) {
+        if (!fl.isDirectory()) {
+            return fl.length();
+        }
+        long rsl = 0;
+        for (File subfl : fl.listFiles()) {
+            if (subfl.isDirectory()) {
+                rsl += getFileSize(subfl);
+            } else {
+                rsl += subfl.length();
+            }
+        }
+        return rsl;
+    }
+
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        File file = new File("c:/projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.print("Name: " + file.getAbsolutePath() + ", ");
-        System.out.println(String.format("size: %s", file.getTotalSpace()));
-        /*
+        System.out.println(
+                String.format("Name: %s, size: %s", file.getAbsolutePath(), getFileSize(file))
+        );
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getAbsoluteFile());
+            System.out.println(
+                    String.format("Name: %s, size: %s", subfile.getAbsolutePath(), getFileSize(subfile))
+            );
         }
-        */
     }
 }
