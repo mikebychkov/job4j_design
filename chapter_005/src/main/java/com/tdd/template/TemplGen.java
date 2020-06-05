7d587ccefa10007ac0557b161ea89bf8463c7625
mikebychkov <mihailbichkov@yandex.ru>
2020-06-06 00:15:10 +0500

    3. Шаблонизатор. [#276480]

---
+++ chapter_005/src/main/java/com/tdd/template/TemplGen.java
@@ -0,0 +1,10 @@
+package com.tdd.template;
+
+import java.util.Map;
+
+public class TemplGen implements Generator {
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
---
+++ chapter_005/src/test/java/com/tdd/template/GeneratorTest.java
@@ -0,0 +1,43 @@
+package com.tdd.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Robocop");
+        params.put("subject", "Murphy");
+        Generator gen = new TemplGen();
+        String rsl = gen.produce(template, params);
+        assertThat(rsl, is("I am a Robocop, Who are Murphy?"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notEnoughParams() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Robocop");
+        Generator gen = new TemplGen();
+        String rsl = gen.produce(template, params);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooManyParams() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Robocop");
+        params.put("subject", "Murphy");
+        params.put("gun", "Model-T1000XL");
+        Generator gen = new TemplGen();
+        String rsl = gen.produce(template, params);
+    }
+}
